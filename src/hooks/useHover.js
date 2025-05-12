bfbfcab484e3a7964f3d727fdbc23552a3687002
sentares <aydararzybekov2002@mail.ru>
2025-05-12 14:39:07 +0600

    fix(useHover):initializing handleMouse function outside the useEffect and add the function to dependency array

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,15 +1,16 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 export function useHover() {
 	const [hovered, setHovered] = useState(false)
 	const ref = useRef(null)
 
+	const handleMouseOver = useCallback(() => setHovered(true), [])
+	const handleMouseOut = useCallback(() => setHovered(false), [])
+
 	useEffect(() => {
 		const node = ref.current
-		if (!node) return
 
-		const handleMouseOver = () => setHovered(true)
-		const handleMouseOut = () => setHovered(false)
+		if (!node) return
 
 		node.addEventListener('mouseover', handleMouseOver)
 		node.addEventListener('mouseout', handleMouseOut)
@@ -18,7 +19,7 @@ export function useHover() {
 			node.removeEventListener('mouseover', handleMouseOver)
 			node.removeEventListener('mouseout', handleMouseOut)
 		}
-	}, [ref])
+	}, [handleMouseOver, handleMouseOut])
 
 	return { hovered, ref }
 }
